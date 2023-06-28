bc9f1b3c03a39a4aef4ed264b09ed8ac4cf6ee6c
SirRoti <timgoellner6@gmail.com>
2023-06-28 19:42:02 +0200

    Set apear animation logic to a global scope

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,30 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+addEventListener("turbo:load", (event) => appearAnimations())
+addEventListener("scroll", (event) => appearAnimations())
+
+function appearAnimations() {
+  const appearElements = document.querySelectorAll('.animation_appear')
+  const fadeInElements = document.querySelectorAll('.animation_fadeIn')
+
+  appearElements.forEach(element => {
+    const elementRect = element.getBoundingClientRect()
+
+    if (elementRect.top >= 0 && elementRect.left >= 0 && elementRect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && elementRect.right <= (window.innerWidth || document.documentElement.clientWidth)) {
+      element.classList.remove('animation_appear')
+      element.classList.add('appear')
+    }
+  })
+
+  fadeInElements.forEach(element => {
+    const elementRect = element.getBoundingClientRect()
+
+    if (elementRect.top >= 0 && elementRect.left >= 0 && elementRect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && elementRect.right <= (window.innerWidth || document.documentElement.clientWidth)) {
+      element.classList.remove('animation_fadeIn')
+      element.classList.add('fadeIn')
+    }
+  })
+}
+
